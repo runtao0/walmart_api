4c2634f83752323b424e6a1170c5dc3e5f569769
Runtao0 <ry2228@columbia.edu>
2017-07-28 00:10:45 -0400

    npm and webpack files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './lib/main.jsx',
+  output: {
+    path: '/Users/runtao/Desktop/fohr/lib/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
