284fb05bbb2d7f2a07703a377dc04bdb141797cf
Runtao0 <ry2228@columbia.edu>
2017-08-03 15:44:09 -0400

    fix confirm modal css

--- lib/components/modals/confirm_modal.jsx
+++ lib/components/modals/confirm_modal.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const ConfirmModal = ({ message, confirmModal, setModal}) => {
     return (
         <section id="confirm-modal">
-            <div className="confirm-message">{message}</div>
-            <div className="confirm-buttons">
-                <button onClick={ () => setModal() }>Cancel</button>
-                <button onClick={ () => confirmModal() }>Confirm</button>
+            <div>
+                <div className="confirm-message">{message}</div>
+                <div className="confirm-buttons">
+                    <button onClick={ () => setModal() }>Cancel</button>
+                    <button onClick={ () => confirmModal() }>Confirm</button>
+                </div>
             </div>
         </section>
     )
