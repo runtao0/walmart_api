7e0d47fb5de9cc73f7fd58eaef5e30fec1d2c111
Runtao0 <ry2228@columbia.edu>
2017-08-03 00:26:13 -0400

    update walmart query table to include everything except query through local

--- lib/components/walmart_query_table/walmart_query_table.jsx
+++ lib/components/walmart_query_table/walmart_query_table.jsx
@@ -1,5 +1,6 @@
 import React, { Proptypes } from 'react';
-import QueryBarContainer from '../query_bar/query_bar_container'
+import QueryBarContainer from '../query_bar/query_bar_container';
+import TableContainer from '../table/table_container';
 
 class WalmartQueryTable extends React.Component {
     constructor(props) {
@@ -11,18 +12,45 @@ class WalmartQueryTable extends React.Component {
         this.setModal = this.setModal.bind(this);
     }
 
+
     setModal() {
         const { modal } = this.state;
         this.setState({ modal: !modal })
     }
 
+
+
+    noProducts () {
+        return (
+            <h3>There are no products loaded</h3>
+        )
+    }
+
     render() {
+        const { error, items } = this.props;
+        const areThereItems = Object.keys(items).length > 0;
+
         return (
             <div>
                 <QueryBarContainer/>
+                <section className="messages">
+                    { error &&
+                        <h2>{ error }</h2>
+                    }
+                    { !areThereItems &&
+                        this.noProducts()
+                    }
+                </section>
+                { areThereItems &&
+                    <section className="table-container">
+                        <TableContainer/>
+                    </section>
+                }
             </div>
         )
     }
 }
+// { this.displayQueryHeader() }
 
+// {  displayQuery Header }
 export default WalmartQueryTable;
